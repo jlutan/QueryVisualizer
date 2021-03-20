54aab1867eab9a393c6490ebd5d5618ed667e434
Jesse <jlutannn@gmail.com>
2021-03-19 19:04:19 -0700

    Added to user interface

--- healthnode.py
+++ healthnode.py
@@ -3,6 +3,7 @@ import streamlit as st
 import pandas as pd
 from dotenv import load_dotenv
 from os import getenv
+import flat_table
 
 load_dotenv()
 TG_HOST = "https://healthnode.i.tgcloud.io"
@@ -29,81 +30,46 @@ st.title("HealthNode Graph")
 
 st.write("Welcome to my first TigerGraph and Streamlit project connecting a web application to a graph database and visualizing query data.")
 
-st.header("Currently Installed Queries:")
-if st.checkbox("Show queries:"):
-    st.write(graph.getInstalledQueries())
+sb = st.sidebar
+selection = sb.selectbox("Function Select", ["Run an Installed Query", "Write a Custom Query"])
 
-st.header("Query Example:")
+data = pass
 
+if selection == "Run an Installed Query":
+    st.header("Run an Installed Query")
+    st.write("This interface will allow you to run any queries currently installed in the cloud database. Pick a query, specify arguments on-the-fly, and view the output as a table, chart, or map.")
+    
+    st.subheader("Currently Installed Queries")
+    if st.checkbox("Show queries"):
+        df = pd.DataFrame(graph.getInstalledQueries())
+        data = flat_table.normalize(df)
+        st.table(data)
+    
+    st.header("Query Example")
 
-query = graph.runInstalledQuery("topSideEffectsForTopDrugs")
-# params="companyName=PFIZER&k=5&role=PS")
+    # run installed query
+    query = graph.runInstalledQuery("topSideEffectsForTopDrugs", params="companyName=PFIZER&k=5&role=PS")
 
+    # convert raw query to pandas Data Frame
+    df = pd.DataFrame(query[0]["TopDrugs"])
+    data = flat_table.normalize(df)
 
-# query = graph.runInterpretedQuery(
-#     """
-#     INTERPRET QUERY (STRING companyName, INT k, STRING role) FOR GRAPH faers {
-#         # Define a heap structure which sorts the reaction map (below) by count
-#         TYPEDEF TUPLE<STRING name, INT cnt> tally;
-#         HeapAccum<tally>(k, cnt DESC) @topReactions;
-            
-#         # Keep count of how many times each reaction or drug is mentioned.
-#         ListAccum<STRING> @reactionList;
-#         SumAccum<INT> @numCases;
-#         MapAccum<STRING, INT> @reactionTally;
+    # print to streamlit app
+    st.table(data)
+elif selection == "Write a Custom Query":
+    pass
 
-#     # 1. Find all the cases where the given pharma company is the 'mfr_sndr'
-#     Company ={PharmaCompany.*};
-#     Cases = SELECT c
-#         FROM Company:s -(relatedTo:e)-> ReportedCase:c
-#         WHERE s.mfr_sndr == companyName
-#         ;
-
-#         # 2. For each case, attach a list of its reactions
-#         Tally = SELECT r
-#         FROM Cases:c -(hasReactions:e)-> Reaction:r
-#         ACCUM c.@reactionList += r.pt;
-            
-#         # 3. Find all the drug sequences for the selected cases, and transfer
-#         #    the reaction list to the drug sequence.
-#         DrugSeqs = SELECT ds
-#         FROM Cases:c -(hasSequences:e)-> DrugSequence:ds
-#         WHERE (role == "" OR ds.role_cod == role)
-#             ACCUM ds.@reactionList = c.@reactionList
-#     ;
-        
-#         # 4. Count the occurences of each drug mentioned in each drug sequence.
-#         #    Also count the occurences of each reaction.
-#         TopDrugs = SELECT d
-#         FROM DrugSeqs:ds -(hasDrugs:e)-> Drug:d
-#         ACCUM d.@numCases += 1,
-#         FOREACH reaction in ds.@reactionList DO
-#                 d.@reactionTally += (reaction -> 1)
-#             END
-#         ORDER BY d.@numCases DESC
-#         LIMIT k
-#         ;
-            
-#         # 5. Find only the Top K side effects for each selected Drug
-#         TopDrugs = SELECT d
-#             FROM TopDrugs:d 
-#             ACCUM
-#             FOREACH (reaction, cnt) IN d.@reactionTally DO
-#                 d.@topReactions += tally(reaction,cnt)
-#             END
-#             ORDER BY d.@numCases DESC
-#             ;
-        
-#         PRINT TopDrugs[TopDrugs.prod_ai, TopDrugs.@numCases, TopDrugs.@topReactions];
-#     }
-#     """,
-#     params={
-#         "companyName": "PFIZER",
-#         "k": 5,
-#         "role": "PS"
-#     }
-# )
-
-df = pd.DataFrame(query[0]["TopDrugs"])
-
-st.table(df)
+st.header("Output")
+data_format = st.selectbox("Output Format", ["Table", "Chart", "Map"])
+if data_format == "Table":
+    st.table(data)
+elif data_format == "Chart":
+    chart_format = st.selectbox("Chart Type", ["Line Chart", "Area Chart", "Bar Chart"])
+    if chart_format == "Line Chart":
+        st.line_chart()
+    elif chart_format == "Area Chart":
+        pass
+    elif chart_format == "Bar Chart":
+        pass 
+elif data_format == "Map":
+    pass
